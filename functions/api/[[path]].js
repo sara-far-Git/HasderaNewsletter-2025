f25dce7e890d836ac4b97680ac32d89b13b5225d
sara-far-Git <s3296900@gmail.com>
2026-01-13 11:34:58 +0200

    Fix api/[[path]].js: properly forward Authorization header

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -20,10 +20,19 @@ export async function onRequest({ request, params }) {
   const targetUrl = new URL(`/api/${rest}`, BACKEND_ORIGIN);
   targetUrl.search = incomingUrl.search;
 
-  // העברת כל ה-headers, method, ו-body
+  // בניית headers חדשים - רק אלה שצריך להעביר
+  const headers = new Headers();
+  const authHeader = request.headers.get('Authorization');
+  if (authHeader) headers.set('Authorization', authHeader);
+  const contentType = request.headers.get('Content-Type');
+  if (contentType) headers.set('Content-Type', contentType);
+  const accept = request.headers.get('Accept');
+  if (accept) headers.set('Accept', accept);
+
+  // העברת method, headers, ו-body
   const proxiedRequest = new Request(targetUrl.toString(), {
     method: request.method,
-    headers: request.headers,
+    headers: headers,
     body: request.body,
   });
 
